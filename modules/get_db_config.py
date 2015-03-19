7a37f9335714da71b61fab540defa14fef5f8299
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2015-03-19 13:31:05 +0100

    smallfix

--- modules/get_db_config.py
+++ modules/get_db_config.py
@@ -1,4 +1,7 @@
-config = dict(shebanq_user='shebanq', shebanq_host='localhost')
+config = dict(shebanq_user='shebanq')
 config_path = '/opt/emdros/cfg/mql.cfg'
 with open(config_path) as p:
     config['shebanq_passwd'] = p.read().rstrip('\n')
+config_path = '/opt/emdros/cfg/host.cfg'
+with open(config_path) as p:
+    config['shebanq_host'] = p.read().rstrip('\n')
