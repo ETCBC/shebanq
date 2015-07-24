ca4fc159e96fd2f97b06cf67b86f48db2d1485f9
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2015-07-24 20:44:49 +0200

    tools

---
+++ static/docs/tools/shebanq/images/rhtext_def.js
@@ -0,0 +1,297 @@
+
+function set_toggle_txt_i() {
+    $("#toggle_txt_i").attr('checked', false);
+    $("#toggle_txt_i").change(function() {
+        $('p.i').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_txt_p() {
+    $("#toggle_txt_p").attr('checked', true);
+    $("#toggle_txt_p").change(function() {
+        $('p.p').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_ht() {
+    $("#toggle_ht").attr('checked', true);
+    $("#toggle_ht").change(function() {
+        $('.ht').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_hl() {
+    $("#toggle_hl").attr('checked', true);
+    $("#toggle_hl").change(function() {
+        $('.hl').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_tt() {
+    $("#toggle_tt").attr('checked', true);
+    $("#toggle_tt").change(function() {
+        $('.tt').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_tl() {
+    $("#toggle_tl").attr('checked', true);
+    $("#toggle_tl").change(function() {
+        $('.tl').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_gl() {
+    $("#toggle_gl").attr('checked', true);
+    $("#toggle_gl").change(function() {
+        $('.gl').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd1() {
+    $("#toggle_wd1").attr('checked', true);
+    $("#toggle_wd1").change(function() {
+        $('.wd1').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd1_subpos() {
+    $("#toggle_wd1_subpos").attr('checked', true);
+    $("#toggle_wd1_subpos").change(function() {
+        $('.wd1_subpos').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd1_pos() {
+    $("#toggle_wd1_pos").attr('checked', true);
+    $("#toggle_wd1_pos").change(function() {
+        $('.wd1_pos').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd1_lang() {
+    $("#toggle_wd1_lang").attr('checked', true);
+    $("#toggle_wd1_lang").change(function() {
+        $('.wd1_lang').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd1_n() {
+    $("#toggle_wd1_n").attr('checked', true);
+    $("#toggle_wd1_n").change(function() {
+        $('.wd1_n').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2() {
+    $("#toggle_wd2").attr('checked', true);
+    $("#toggle_wd2").change(function() {
+        $('.wd2').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2_gender() {
+    $("#toggle_wd2_gender").attr('checked', true);
+    $("#toggle_wd2_gender").change(function() {
+        $('.wd2_gender').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2_gnumber() {
+    $("#toggle_wd2_gnumber").attr('checked', true);
+    $("#toggle_wd2_gnumber").change(function() {
+        $('.wd2_gnumber').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2_person() {
+    $("#toggle_wd2_person").attr('checked', true);
+    $("#toggle_wd2_person").change(function() {
+        $('.wd2_person').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2_state() {
+    $("#toggle_wd2_state").attr('checked', true);
+    $("#toggle_wd2_state").change(function() {
+        $('.wd2_state').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2_tense() {
+    $("#toggle_wd2_tense").attr('checked', true);
+    $("#toggle_wd2_tense").change(function() {
+        $('.wd2_tense').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_wd2_stem() {
+    $("#toggle_wd2_stem").attr('checked', true);
+    $("#toggle_wd2_stem").change(function() {
+        $('.wd2_stem').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_sp() {
+    $("#toggle_sp").attr('checked', true);
+    $("#toggle_sp").change(function() {
+        $('.sp').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_sp_rela() {
+    $("#toggle_sp_rela").attr('checked', true);
+    $("#toggle_sp_rela").change(function() {
+        $('.sp_rela').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_sp_n() {
+    $("#toggle_sp_n").attr('checked', true);
+    $("#toggle_sp_n").change(function() {
+        $('.sp_n').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_ph() {
+    $("#toggle_ph").attr('checked', true);
+    $("#toggle_ph").change(function() {
+        $('.ph').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_ph_det() {
+    $("#toggle_ph_det").attr('checked', true);
+    $("#toggle_ph_det").change(function() {
+        $('.ph_det').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_ph_fun() {
+    $("#toggle_ph_fun").attr('checked', true);
+    $("#toggle_ph_fun").change(function() {
+        $('.ph_fun').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_ph_typ() {
+    $("#toggle_ph_typ").attr('checked', true);
+    $("#toggle_ph_typ").change(function() {
+        $('.ph_typ').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_ph_n() {
+    $("#toggle_ph_n").attr('checked', true);
+    $("#toggle_ph_n").change(function() {
+        $('.ph_n').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_cl() {
+    $("#toggle_cl").attr('checked', true);
+    $("#toggle_cl").change(function() {
+        $('.cl').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_cl_dom() {
+    $("#toggle_cl_dom").attr('checked', true);
+    $("#toggle_cl_dom").change(function() {
+        $('.cl_dom').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_cl_typ() {
+    $("#toggle_cl_typ").attr('checked', true);
+    $("#toggle_cl_typ").change(function() {
+        $('.cl_typ').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_cl_n() {
+    $("#toggle_cl_n").attr('checked', true);
+    $("#toggle_cl_n").change(function() {
+        $('.cl_n').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_sn() {
+    $("#toggle_sn").attr('checked', true);
+    $("#toggle_sn").change(function() {
+        $('.sn').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
+
+function set_toggle_sn_n() {
+    $("#toggle_sn_n").attr('checked', true);
+    $("#toggle_sn_n").change(function() {
+        $('.sn_n').each(function () {
+            $( this ).toggle();
+        });
+    });
+}
