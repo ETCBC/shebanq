23cf135116ff912d43f7190dc2512620b7677931
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2015-11-05 08:54:01 +0100

    other server

--- models/menu.py
+++ models/menu.py
@@ -35,12 +35,12 @@ response.logo = A(IMG(_src=URL('static', 'images/shebanq_logo_small.png')),
                   )
 
 
-served_on = request.env.remote_addr
+served_on = request.env.HTTP_HOST
 on_local = False
 on_prod = False
 on_devel = False
 if served_on == '127.0.0.1': on_local = True
-elif served_on == 'PPVJ003': on_prod = True
+elif served_on == '37.48.78.25': on_prod = True
 elif served_on == 'clarin11.dans.knaw.nl': on_devel = True
 
 #response.title = request.application.replace('_',' ').title()
