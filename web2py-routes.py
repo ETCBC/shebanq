38588b6418e687d2045ae08b6f77ef3c1403b5b2
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2017-01-10 14:59:38 +0100

    code and docs update

---
+++ web2py-routes.py
@@ -0,0 +1,13 @@
+# at production: copy this file to /opt/web-apps/web2py as routes.py
+routers = dict(
+    BASE = dict(
+        default_application='shebanq',
+        root_static=[
+            'robots.txt',
+            'favicon.ico',
+            'apple-touch-icon.png',
+            'google9e12b65b9e77c1da.html',
+        ],
+    )
+)
+
