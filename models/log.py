72d494ac447c19fb9cb02a7e10636469dad01bd7
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2021-10-18 15:12:30 +0200

    check logging

--- models/log.py
+++ models/log.py
@@ -6,4 +6,4 @@ from gluon import current
 logger = logging.getLogger("web2py.app.shebanq")
 logger.setLevel(logging.WARN)
 current.logger = logger
-logger.debug("XXXXXXXXXXXXXX TEST XXXXXXXXXXXXXXXXXXX")
+logger.error("XXXXXXXXXXXXXX TEST XXXXXXXXXXXXXXXXXXX")
